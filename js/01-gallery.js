db85df6757f1f87ee9e56b7822acf7414b6b2d16
ruchey34 <me.oleg1@gmail.com>
2022-11-12 13:41:46 +0200

    1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,55 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkUp = createGalleryMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
+
+console.log(createGalleryMarkup(galleryItems));
+
+paletteContainer.addEventListener('click', onPaletteContainerClick );
+
+
+ function createGalleryMarkup(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+          <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+              />
+            </a>
+          </div>
+        `;
+      })
+      .join('');
+     
+  }
+
+let modalWindow;
+let onClickClose;
+  function onPaletteContainerClick (evt){
+    evt.preventDefault();
+
+if(!evt.target.classList.contains('gallery__image')){
+    return;
+}
+modalWindow = basicLightbox.create(
+  ` <img src="${evt.target.dataset.source}"/>`
+);
+modalWindow.show(onClickClose);
+
+// document.div.classList.add('basicLightbox');
+window.addEventListener('keydown', onEscClose);
+
+  }
+
+  function onEscClose (event){
+    modalWindow.close();
+  }
